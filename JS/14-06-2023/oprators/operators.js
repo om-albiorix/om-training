3dbac3e1887dea97692c3c176f283174d2287c05
Harry <omsolanki3698@gmail.com>
2023-06-14 14:45:19 +0530

    js practise code

---
+++ JS/14-06-2023/oprators/operators.js
@@ -0,0 +1,32 @@
+//assignment operators
+let x = 20;
+//addition operators
+let y = 20;
+let z = 21;
+let a = y + z;
+//javascript multiplication
+let e = 5, r = 3;
+let h = e * r;
+//arithmatic operators
+let om = 20 + (20 * 20);
+//Assignment Operators
+let d = 5;
+d += 5;
+console.log(d);
+// example
+let text1 = "1";
+let text2 = 20;
+let text3 = text1 + " " + text2;
+document.getElementById("demo").innerHTML = text3;
+alert(typeof (text1 + text2))
+alert(text1 + text2)
+
+// assignment operators in detail
+//addition opertors
+let num = 5
+num += 5
+console.log(num);
+//division operators
+let devide = 5
+devide /= 5
+console.log(devide);
